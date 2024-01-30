9faf2d4d994d62130bb4e713216a3e902497cf08
everton1707 <everton.leema@gmail.com>
2024-01-29 22:37:26 -0300

    Criando Migrations e banco de dados

--- back/src/ProEventos.API/Controllers/EventoController.cs
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,7 +14,7 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
         public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
+               /* new Evento(){
                 EventoId = 1,
                 Tema = "Angular 11 e dotnet 5",
                 Local = "taubate-SP",
@@ -30,22 +31,24 @@ namespace ProEventos.API.Controllers
                 QtdPessoas = 250,
                 DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
                 ImagemURL = "foto2.png"
-                }
+                }*/
             };
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
          
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id );
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id );
         }
         [HttpPost]
         public string Post()
