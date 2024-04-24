6093147084ac309c7ebadfde1fb53e221185a01d
everton1707 <everton.leema@gmail.com>
2024-04-23 23:15:42 -0300

    Seção 6: .NET Core - Organizando Camadas

--- back/src/ProEventos.API/Startup.cs
+++ back/src/ProEventos.API/Startup.cs
@@ -5,7 +5,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using ProEventos.Application;
+using ProEventos.Application.Contratos;
 using ProEventos.Persistence;
+using ProEventos.Persistence.Contextos;
+using ProEventos.Persistence.Contratos;
+
 namespace ProEventos.API
 {
     public class Startup
@@ -23,7 +28,13 @@ namespace ProEventos.API
             services.AddDbContext<ProEventosContext>(
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(
+                x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore //quebrando looping infinito
+            );
+            services.AddScoped<IEventoService, EventoService>();
+            services.AddScoped<IGeralPersist, GeralPersist>();
+            services.AddScoped<IEventoPersist, EventoPersist>();
+                       
             services.AddCors();
             services.AddSwaggerGen(c =>
             {
---
+++ back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contextos
+{
+
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) :
+        base(options){ }
+        public DbSet<Evento> Eventos { get;set;}
+        public DbSet<Lote> Lotes { get;set;}
+        public DbSet<Palestrante> Palestrantes { get;set;}
+        public DbSet<PalestranteEvento> PalestrantesEventos { get;set;}
+        public DbSet<RedeSocial> RedesSociais { get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new {PE.EventoId, PE.PalestranteId});
+            
+            modelBuilder.Entity<Evento>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Evento)//cada rede social com um evento
+                .OnDelete(DeleteBehavior.Cascade);//deletar em cascata 
+            modelBuilder.Entity<Palestrante>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Palestrante)//cada rede social de um palestrante
+                .OnDelete(DeleteBehavior.Cascade);//deletar em cascata 
+        }
+        
+    }
+}
